29bfff47192e76bb1e43c1930d21e9a8b67803cb
Joe D'Alessandro <joe@thejoeshow.net>
2020-12-24 14:15:09 -0500

    day 1 part 2

--- day1.js
+++ day1.js
@@ -4,14 +4,19 @@ const data = require('./data/day1.js');
 
 const lookup = data.reduce((h, num) => {
   return h[num] = num, h;
-}, {})
+}, {});
 
-for (let i = 0; i < data.length; i++) {
-  let diff = 2020 - data[i];
+let numbers = [];
+let found = false;
 
-  if (lookup[diff]) {
-    console.log(`one/two -> ${data[i]}/${lookup[diff]}`);
-    console.log(data[i] * lookup[diff]);
-    break;
+for (let i = 0; i < data.length && !found; i++) {
+  for (let j = 0; j < data.slice(i).length && !found; j++) {
+    let diff = 2020 - (data[i] + data[j]);
+
+    if (lookup[diff]) {
+      console.log(`one/two/three -> ${data[i]}/${data[j]}/${lookup[diff]}`);
+      console.log(data[i] * data[j] * lookup[diff]);
+      found = true;
+    }
   }
 }
